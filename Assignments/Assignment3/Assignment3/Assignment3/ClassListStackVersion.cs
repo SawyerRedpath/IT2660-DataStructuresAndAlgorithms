87d8e6cd56f395084543d1054763b3b8372550dd
Sawyer R <sawyerbruceredpath@gmail.com>
2020-02-16 16:30:59 -0500

    Finished assignment 3

---
+++ Assignments/Assignment3/Assignment3/Assignment3/ClassListStackVersion.cs
@@ -0,0 +1,62 @@
+﻿namespace Assignment3
+{
+    internal class ClassListStackVersion
+    {
+        public Student[] students { get; set; }
+        public int currentStudentsInClass { get; set; }
+
+        public ClassListStackVersion(Student[] students)
+        {
+            this.students = students;
+            this.currentStudentsInClass = this.students.Length;
+        }
+
+        public ClassListStackVersion(Student student)
+        {
+            this.students = new Student[1];
+            this.students[0] = student;
+            this.currentStudentsInClass = 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return currentStudentsInClass == 0;
+        }
+
+        public Student Peek()
+        {
+            return this.students[currentStudentsInClass - 1];
+        }
+
+        public void Push(Student student)
+        {
+            if (currentStudentsInClass == students.Length)
+            {
+                Resize(2 * students.Length);
+            }
+            students[currentStudentsInClass++] = student;
+        }
+
+        private void Resize(int newCapacity)
+        {
+            Student[] copy = new Student[newCapacity];
+            for (int i = 0; i < currentStudentsInClass; i++)
+            {
+                copy[i] = students[i];
+            }
+
+            this.students = copy;
+        }
+
+        public Student Pop()
+        {
+            Student student = students[--currentStudentsInClass];
+            students[currentStudentsInClass] = null;
+            if (currentStudentsInClass > 0 && currentStudentsInClass == students.Length / 4)
+            {
+                Resize(students.Length / 2);
+            }
+            return student;
+        }
+    }
+}
---
+++ Assignments/Assignment3/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ClassListStackVersion algorithmClass = new ClassListStackVersion(new Student(0, "FirstName", "LastName", 3.0));
+
+            Console.WriteLine("Class begins with 1 student \n");
+
+            Console.WriteLine("Push students\n");
+            for (int i = 1; i < 16; i++)
+            {
+                algorithmClass.Push(new Student(i, "FirstName" + i.ToString(), "LastName" + i.ToString(), 3.0));
+
+                Console.WriteLine("Student " + (i + 1).ToString() + " pushed");
+                Console.WriteLine("Current array size = " + algorithmClass.students.Length + "\n");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pop students");
+
+            for(int i = 0; i < 14; i++)
+            {
+                Student student = algorithmClass.Pop();
+                Console.WriteLine("Stack student #" + (algorithmClass.currentStudentsInClass +1).ToString() + " popped");
+                Console.WriteLine("Current stack size = " + algorithmClass.students.Length + "\n");
+            }
+        }
+    }
+}
